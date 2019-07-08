b60dc4ca685e0e7582a884b08165853594790ea8
DeadRomance <DeadRomance3@163.com>
2019-07-08 12:36:37 +0800

    新增滚动区域组件

---
+++ src/components/s-common/s-scroll-view.vue
@@ -0,0 +1,53 @@
+<template>
+  <div :class="SScrollClassObj">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    scroll: {
+      type: String,
+      default: ""
+    },
+  },
+
+  computed: {
+    SScrollClassObj: function() {
+      let y = this.scroll.toLocaleLowerCase() == "y" ? true : false;
+      let x = this.scroll.toLocaleLowerCase() == "x" ? true : false;
+
+      return {
+        "s-scroll-view-default": true,
+        scrollY: y,
+        scrollX: x
+      };
+    },
+    // SScrollStyleObj: function() {
+    //   return {
+    //       height: this.height + 'px'
+    //   };
+    // }
+  }
+};
+</script>
+
+<style lang="less">
+.s-scroll-view-default {
+  width: 100%;
+  height: 100%;
+  overflow-y: hidden;
+  overflow-x: hidden;
+}
+.scrollY {
+  overflow-y: scroll;
+  -webkit-overflow-scrolling: touch;
+}
+.scrollX {
+  overflow-x: scroll;
+  -webkit-overflow-scrolling: touch;
+  display: flex;
+}
+
+</style>
--- src/utils/S-component.js
+++ src/utils/S-component.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 
 
 
-// ????
+// 头部区域
 import SHeader from "../components/s-common/S-header.vue";
-// ?????
+// 底部区域
 import SFooter from "../components/s-common/S-footer.vue";
+// 内容区域
 import SContainer from "../components/s-common/S-container.vue";
+// 滚动区域
+import SScrollView from '../components/s-common/s-scroll-view.vue'
 
 
 
@@ -17,6 +20,7 @@ import SContainer from "../components/s-common/S-container.vue";
 
 
 
-Vue.component('SHeader',SHeader)
-Vue.component('SFooter',SFooter)
+Vue.component('SHeader', SHeader)
+Vue.component('SFooter', SFooter)
 Vue.component('SContainer', SContainer)
+Vue.component('SScrollView', SScrollView)
